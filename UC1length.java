1e806d5d83e45ab207295da6e3f9e65d5cde0f7c
PranayKawale <83820439+PranayKawale@users.noreply.github.com>
2021-07-09 16:28:16 +0530

    length of line calculated

---
+++ UC1length.java
@@ -0,0 +1,20 @@
+
+public class UC1length {
+	public static void main(String[] args) 
+	
+	{
+		
+		  double x1=1;
+		  double y1=3;
+		  double x2=4;
+		  double y2=5;
+		  double Distance;
+		  
+		  
+		  Distance = Math.sqrt(Math.pow(x2 - x1, 2)) + (Math.pow(y2 - y1, 2));
+		  System.out.println("The length of line :: "  +Distance);
+
+		
+	}
+
+}
